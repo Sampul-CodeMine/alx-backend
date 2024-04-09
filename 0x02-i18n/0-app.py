b68eb583c409838ff5b5b6c12917923b1e84e0c6
Sampul-CodeMine <studysampul@gmail.com>
2024-04-09 17:11:21 +0100

    Project Structure and solution to Task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This is a Basic Flask App to create a simple route
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """This is a basic route to the homepage
+
+    Returns:
+        str: Returns a string in the index page
+    """
+    return render_template('0-index.html', title='Welcome to Holberton')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
